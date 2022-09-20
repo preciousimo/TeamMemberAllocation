365165625a1b36b99e5503c8ac75e8da7562047f
preciousimo <preciousimoniakemu@gmail.com>
2022-09-20 17:08:29 +0100

    Put Responsibilities Into Functional Components

--- src/Employees.js
+++ src/Employees.js
@@ -1,5 +1,5 @@
-import femaleProfile from "./images/femaleProfile.jpg";
-import maleProfile from "./images/maleProfile.jpg";
+import Teams from "./Teams";
+import TeamMembers from "./TeamMembers";
 
 const Employees = ({
   employees,
@@ -11,45 +11,20 @@ const Employees = ({
     <main className="container">
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-6">
-          <select
-            className="form-select form-select-lg"
-            value={selectedTeam}
-            onChange={handleTeamSelectionChange}
-          >
-            <option value="TeamA">TeamA</option>
-            <option value="TeamB">TeamB</option>
-            <option value="TeamC">TeamC</option>
-            <option value="TeamD">TeamD</option>
-          </select>
+          <Teams
+            selectedTeam={selectedTeam}
+            handleTeamSelectionChange={handleTeamSelectionChange}
+          />
         </div>
       </div>
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-8">
           <div className="card-collection">
-            {employees.map((employee) => (
-              <div key={employee.id}
-                id={employee.id}
-                className={
-                  employee.teamName === selectedTeam
-                    ? "card m-2 standout"
-                    : "card m-2"
-                }
-                style={{ cursor: " pointer " }}
-                onClick={handleEmployeeCardClick}
-              >
-                {employee.gender === "male" ? (
-                  <img src={maleProfile} className="card-img-top" />
-                ) : (
-                  <img src={femaleProfile} className="card-img-top" />
-                )}
-                <div className="card-body">
-                  <h5 className="card-title">Full Name: {employee.fullName}</h5>
-                  <p className="card-text">
-                    <b>Designation:</b> {employee.designation}
-                  </p>
-                </div>
-              </div>
-            ))}
+            <TeamMembers
+              employees={employees}
+              handleEmployeeCardClick={handleEmployeeCardClick}
+              selectedTeam={selectedTeam}
+            />
           </div>
         </div>
       </div>
--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = ({selectedTeam, teamMemberCount}) => {
             <div className="row justify-content-center mt-3 mb-4">
                 <div>
                     <h1>Team Member Allocation</h1>
-                    <h3>{selectedTeam} has {teamMemberCount} Members</h3>
+                    <h3>{selectedTeam} has {teamMemberCount} {teamMemberCount===1?"member":"members"}</h3>
                 </div>
             </div>
         </header>
